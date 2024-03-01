5d91893510f40bbfb74393d08983b35818b55fe8
kaniou <kaniou@kku.ac.th>
2024-03-01 13:16:03 +0700

    เพิ่ม suspense

--- app/thaid-callback/page.tsx
+++ app/thaid-callback/page.tsx
@@ -1,10 +1,13 @@
 import CallbackTable from "@/components/CallbackThaiD"
+import { Suspense } from "react"
 
 const Page = () => {
 
   return (
     <>
-      <CallbackTable client_id={process.env.CLIENT_ID as string} client_secret={process.env.CLIENT_SECRET as string} callback_url={process.env.CALLBACK_URL as string} />
+      <Suspense>
+        <CallbackTable client_id={process.env.CLIENT_ID as string} client_secret={process.env.CLIENT_SECRET as string} callback_url={process.env.CALLBACK_URL as string} />
+      </Suspense>
     </>
   )
 }
